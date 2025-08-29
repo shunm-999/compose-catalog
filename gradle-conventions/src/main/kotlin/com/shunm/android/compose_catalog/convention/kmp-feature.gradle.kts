678f50a300457eb64523ebf6e5d968cef5a2ef9c
shunm-999 <shunmiyazaki7812@gmail.com>
2025-08-29 10:11:05 +0900

    add dependencies for Android and common source sets in build configuration

--- gradle-conventions/src/main/kotlin/com/shunm/android/compose_catalog/convention/kmp-feature.gradle.kts
+++ gradle-conventions/src/main/kotlin/com/shunm/android/compose_catalog/convention/kmp-feature.gradle.kts
@@ -2,6 +2,7 @@ package com.shunm.android.compose_catalog.convention
 
 import com.shunm.android.compose_catalog.util.library
 import com.shunm.android.compose_catalog.util.libs
+import gradle.kotlin.dsl.accessors._937c9308645c5e8bdf2065b840746083.implementation
 import org.jetbrains.compose.ExperimentalComposeLibrary
 
 plugins {
@@ -15,16 +16,35 @@ plugins {
 
 kotlin {
     sourceSets {
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.library("androidx-activity-compose"))
+
+            // Navigation3
+            implementation(libs.library("androidx-navigation3-ui"))
+            implementation(libs.library("androidx-navigation3-runtime"))
+            implementation(libs.library("androidx-lifecycle-viewmodel-navigation3"))
+            implementation(libs.library("androidx-material3-adaptive-navigation3"))
+            implementation(libs.library("kotlinx-serialization-core"))
+        }
         commonMain.dependencies {
             // describe shared module
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
 
+            implementation(libs.library("androidx-lifecycle-viewmodelCompose"))
+            implementation(libs.library("androidx-lifecycle-runtimeCompose"))
             implementation(libs.library("kotlinxCollectionsImmutable"))
         }
 
         commonTest.dependencies {
             @OptIn(ExperimentalComposeLibrary::class)
             implementation(compose.uiTest)
-            implementation(libs.library("kotlin-test"))
+            implementation(compose.material3)
         }
 
     }
--- presentation/component/build.gradle.kts
+++ presentation/component/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
     id("com.shunm.android.compose_catalog.convention.kmp-feature")
 }
+
+dependencies {
+    implementation(project(":domain:component"))
+}
