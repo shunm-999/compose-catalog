c43aee6ca10bc2fae6129d71990e7ff9f8ab5afe
shunm-999 <shunmiyazaki7812@gmail.com>
2025-09-01 18:59:14 +0900

    feat: enhance ClSearchAppbar with customizable leading button and placeholder support

--- presentation/component/src/commonMain/kotlin/com/shunm/android/presentation/component/appbar/Appbar.kt
+++ presentation/component/src/commonMain/kotlin/com/shunm/android/presentation/component/appbar/Appbar.kt
@@ -1,35 +1,74 @@
 package com.shunm.android.presentation.component.appbar
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeFlexibleTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumFlexibleTopAppBar
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.SearchBarValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.TopSearchBar
 import androidx.compose.material3.rememberSearchBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.shunm.android.presentation.component.di.Catalogable
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-@Catalogable
 @Composable
 fun ClSearchAppbar(
     onSearch: (String) -> Unit = {},
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingButton: @Composable (AppbarLeadingButtonScope.(isExpanded: Boolean) -> Unit)? = null,
 ) {
+    val coroutineScope = rememberCoroutineScope()
     val searchBarState = rememberSearchBarState()
     val textFieldState = rememberTextFieldState()
+
     TopSearchBar(
         state = searchBarState,
         inputField = {
             SearchBarDefaults.InputField(
                 textFieldState = textFieldState,
                 searchBarState = searchBarState,
-                onSearch = {
+                onSearch = onSearch,
+                placeholder = placeholder,
+                leadingIcon = if (leadingButton == null) {
+                    null
+                } else {
+                    {
+                        leadingButton(
+                            AppbarLeadingButtonScope,
+                            searchBarState.currentValue == SearchBarValue.Expanded,
+                        )
+                    }
+                },
+                trailingIcon = {
+                    IconButton(
+                        modifier = Modifier.size(48.dp),
+                        onClick = {
+                            coroutineScope.launch {
+                                searchBarState.animateToExpanded()
+                            }
+                        },
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search Icon",
+                        )
+                    }
                 },
             )
         },
@@ -137,6 +176,24 @@ fun ClLargeFlexibleAppbar(
             }
         },
         scrollBehavior = scrollBehavior,
+    )
+}
+
+@Preview
+@Composable
+private fun ClSearchAppbarPreview() {
+    ClSearchAppbar(
+        onSearch = {},
+        leadingButton = { isExpanded ->
+            if (isExpanded) {
+                PopBackIcon { }
+            } else {
+                MenuIcon { }
+            }
+        },
+        placeholder = {
+            Text("Search")
+        },
     )
 }
 
